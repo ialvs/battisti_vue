0e42feb22593e6ee4168b5183a9772f024925cca
Ian Alves <ian.santos.alves@hotmail.com>
2022-11-02 21:52:14 -0300

    Até a aula #09

--- vue_cli/projeto_vue/src/components/Info.vue
+++ vue_cli/projeto_vue/src/components/Info.vue
@@ -1,17 +1,30 @@
 <template>
     <div>
-        <p>Busco emprego.</p>
+        <p v-if="hasJob">Estou trabalhando no momento :).</p>
+        <p v-else>Busco emprego.</p>
+
         <p>Utilizo as seguintes tecnologias:</p>
+
         <ul>
             <li>Vue</li>
             <li>Java</li>
             <li>Python</li>
         </ul>
+
+        <p v-show="showEmail">Mande uma mensage para {{ email }}</p>
+
     </div>
 </template>
 
 <script>
 export default {
-    name: 'Info'
+    name: 'Info',
+    data() {
+        return {
+            hasJob: false,
+            showEmail: true,
+            email: "ian@whatever.com"
+        }
+    }
 }
 </script>
