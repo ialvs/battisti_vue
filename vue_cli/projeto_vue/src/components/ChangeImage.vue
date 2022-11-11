e9f04fc12cfc6fe719b05336096eff58751b93cd
Ian Alves <ian.santos.alves@hotmail.com>
2022-11-11 20:20:57 -0300

    Até a aula #18

---
+++ vue_cli/projeto_vue/src/components/ChangeImage.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <button @click="$emit(onChangeImage)">Trocar imagem do perfil</button>
+    </div>
+</template>
+
+<script>
+
+export default{
+    name: 'ChangeImage',
+    emits: ['onChangeImage']
+}
+
+</script>
--- vue_cli/projeto_vue/src/components/MyPicture.vue
+++ vue_cli/projeto_vue/src/components/MyPicture.vue
@@ -1,20 +1,29 @@
 
 <template>
-    <a :href="link"><img :src="avatar" :alt="description" :ref="link"></a>
+    <img :src="avatar" :alt="description" :ref="link">
+    <ChangeImage @on-change-image="switchImage" />
 </template>
 
 <script>
+import ChangeImage from './ChangeImage.vue';
+
 
 export default {
-    name: 'MyPicture',
+    name: "MyPicture",
     data() {
         return {
-            avatar : "/img/IMG_20220827_181848_8558.jpg",
+            avatar: "/img/IMG_20220827_181848_8558.jpg",
             description: "Ian Aves",
             link: "https://github.com/ialvs"
-            }
-        }
-    }
+        };
+    },
+    methods: {
+      switchImage(){
+        this.avatar = "/img/avatar2.png"
+      }  
+    },
+    components: { ChangeImage }
+}
 
 
 </script>
